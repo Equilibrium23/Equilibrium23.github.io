a5218bcb6a0098f4c334fa24e62ddcb978a8ff42
Equilibrium <58709131+Equilibrium23@users.noreply.github.com>
2021-01-03 20:41:55 +0100

    add
    add

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function draw(){
 
     // earth
     var earth = new Image();
-    earth.src = '../images/ziemia2.png'
+    earth.src = 'images/ziemia2.png'
     var earth_offest_x =  earth.width/2;
     var earth_offest_y = earth.height/2;
     var earth_x = a*Math.cos(t);
@@ -47,7 +47,7 @@ function draw(){
 
     //sun 
     var sun = new Image();
-    sun.src = '../images/slonce1.png'
+    sun.src = 'images/slonce1.png'
     var sun_x_offset = sun.width/2;
     var sun_y_offset = sun.height/2;
 
@@ -139,7 +139,7 @@ function drawSecondLaw(){
 
   //sun in focus
   var sun = new Image();
-  sun.src = '../images/slonce1.png'
+  sun.src = 'images/slonce1.png'
   var sun_x_offset = sun.width/2;
   var sun_y_offset = sun.height/2;
   context.drawImage(sun,x-c - sun_x_offset,y-sun_y_offset);
@@ -153,7 +153,7 @@ function drawSecondLaw(){
   var V = Math.sqrt(MI*( (2/R) - (1/a) ));
   document.getElementById('speed').innerHTML = V.toPrecision(3);
   var earth = new Image();
-  earth.src = '../images/ziemia2.png'
+  earth.src = 'images/ziemia2.png'
   var earth_offest_x =  earth.width/2;
   var earth_offest_y = earth.height/2;
   context.drawImage(earth,x+earth_x - earth_offest_x,y+earth_y- earth_offest_y)
@@ -223,51 +223,3 @@ function onlyOne(checkbox) {
       if (item !== checkbox) item.checked = false
   })
 }
-// var sun = new Image();
-// var moon = new Image();
-// var earth = new Image();
-// function init() {
-//   sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-//   moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-//   earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
-//   window.requestAnimationFrame(draw);
-// }
-
-// function draw() {
-//   var ctx = document.getElementById('canvas').getContext('2d');
-
-//   ctx.globalCompositeOperation = 'destination-over';
-//   ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-//   ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-//   ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-//   ctx.save();
-//   ctx.translate(150, 150);
-
-//   // Earth
-//   var time = new Date();
-//   ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-//   ctx.translate(105, 0);
-//   ctx.fillRect(0, -12, 40, 24); // Shadow
-//   ctx.drawImage(earth, -12, -12);
-
-//   // Moon
-//   ctx.save();
-//   ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-//   ctx.translate(0, 28.5);
-//   ctx.drawImage(moon, -3.5, -3.5);
-//   ctx.restore();
-
-//   ctx.restore();
-
-//   ctx.beginPath();
-//   ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-//   ctx.stroke();
-
-//   ctx.drawImage(sun, 0, 0, 300, 300);
-
-//   window.requestAnimationFrame(draw);
-// }
-// function getData(){
-//     init();
-// }
